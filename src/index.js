96322b4f3ad666fd518fafcbc32d8ac0728cc901
Walter Tan <wtan@coursehero.com>
2017-07-26 18:09:45 -0700

    Console log virtualized ul

--- src/index.js
+++ src/index.js
@@ -3,6 +3,15 @@ import diff from 'diff';
 import reconcile from 'reconcile';
 import { mount } from 'dom';
 
+const App = (
+    createNode('ul', {},
+        createNode('li', {}, 'Item one'),
+        createNode('li', {}, 'Item two')
+    )
+);
+
+console.log(App);
+
 // Mount the initial app
 const root = document.getElementById('app');
 // const mounted = mount(App, root);
